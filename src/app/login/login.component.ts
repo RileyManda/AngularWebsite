aa76fbeb3d3adf2abaf7f9e422ed2e15eda01110
RileyManda <rileym@Lilians-MacBook.local>
2018-12-22 03:14:28 +0300

    Fixed form ID

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl,Validators} from '@angular/forms';
 import { Router } from '@angular/router';
-import {AuthService} from '../authservice';
+
 
 @Component({
   selector: 'app-login',
